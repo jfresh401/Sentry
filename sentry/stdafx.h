9bb4d0f2888291bb7a152ac5fc6a29adbae56f4e
Stelio Kontos <37424493+Ste1io@users.noreply.github.com>
2024-02-11 22:38:10 -0600

    add custom printf macros for dbg/sentry output

--- sentry/stdafx.h
+++ sentry/stdafx.h
@@ -7,6 +7,9 @@
 #pragma warning (disable:4482)
 #pragma warning (disable:4172)
 
+#define SDPRINTF_ENABLED             0
+#define SKPRINTF_ENABLED             01
+
 #include <xtl.h>
 #include <stdio.h>
 #include <fstream>
@@ -23,7 +26,20 @@
 #include <cstdlib>
 #include <math.h>
 
+#if SDPRINTF_ENABLED
+#define sdprintf_impl                   DbgPrint
+#define sdprintf(fmt, ...)              sdprintf_impl("[Sentry Dbg]" fmt "\n", __VA_ARGS__)
+#else
+#define sdprintf(fmt, ...)              ((void)__noop)
+#endif
+
+#if SKPRINTF_ENABLED
+#define skprintf_impl                   DbgPrint
+#define skprintf(fmt, ...)              skprintf_impl("[Sentry]" fmt "\n", __VA_ARGS__)
+#else
+#define skprintf(fmt, ...)              ((void)__noop)
+#endif
+
 #include "Utils/Utilities.h"
 
 //game specific
-
