8056bcd3057937a6f9f195786a23a6f8aed384f4
Stelio Kontos <37424493+Ste1io@users.noreply.github.com>
2024-02-14 20:32:39 -0600

    send temp changes individually, no batching
    remove newline prefix on welcome message (exists as visual cue; inconsequential to parser for board, so sending as a dbgprint not a message)
    
    send titleId in uppercase by convention

--- sentry/Main.cpp
+++ sentry/Main.cpp
@@ -17,7 +17,7 @@ namespace sentry {
 	VOID TitleThread() {
 		if (SUCCEEDED(GetXboxInternalIP(g_xboxIP))) {
 			auto octets = g_xboxIP.S_un.S_un_b;
-			SentryMessage("IP: %i.%i.%i.%i", octets.s_b1, octets.s_b2, octets.s_b3, octets.s_b4).Send();
+			SentryMessage("IP: %i.%i.%i.%i", octets.s_b1, octets.s_b2, octets.s_b3, octets.s_b4).Send(); // [Sentry] IP: 255.255.255.255
 		}
 
 		static DWORD titleIdCache = 0;
@@ -28,7 +28,7 @@ namespace sentry {
 				titleIdCache = titleId;
 				if (titleId != NULL) {
 					PLDR_DATA_TABLE_ENTRY moduleHandle = (PLDR_DATA_TABLE_ENTRY)GetModuleHandle(NULL);
-					SentryMessage("TitleID: %08x", titleId).Send();
+					SentryMessage("TitleID: %08X", titleId).Send(); // [Sentry] TitleID: FFFE07D1
 				}
 			}
 
@@ -37,7 +37,7 @@ namespace sentry {
 	}
 
 	VOID TempsThread() {
-		const DWORD delay = 5000;
+		const DWORD delay = 1000;
 		static DWORD lastRun = 0;
 
 		while (!g_terminating) {
@@ -52,19 +52,16 @@ namespace sentry {
 			if (FAILED(GetSystemTemperatures(g_tempValues)))
 				continue;
 
-			SentryMessage msg;
-			const float epsilon = 0.1f; // tolerance threshold of 0.1C; anything less is ignored
+			const float epsilon = 0.1f; // tolerance threshold of 0.1C (highest precision displayed on the board)
 
 			for (auto i = 0; i < SMC_TEMP_TYPE_COUNT; i++) {
 				if (utils::EqualWithTolerance(g_tempValues[i], g_tempValuesCache[i], epsilon))
 					continue;
 
+				// Update this temp's cached value, and send it (no batching)
 				g_tempValuesCache[i] = g_tempValues[i];
-				msg.AppendLine("%s: %.1f", g_tempNames[i], g_tempValues[i]);
+				SentryMessage().AppendLine("%s: %.1f", g_tempNames[i], g_tempValues[i]).Send(); // [Sentry] GPU: 50.1
 			}
-
-			if (!msg.IsEmpty())
-				msg.Send();
 		}
 	}
 
@@ -72,7 +69,8 @@ namespace sentry {
 		g_isDevkit =  *(DWORD*)0x8E038610 & 0x8000 ? FALSE : TRUE;
 		utils::ThreadMe((LPTHREAD_START_ROUTINE)TitleThread, NULL);
 		utils::ThreadMe((LPTHREAD_START_ROUTINE)TempsThread, NULL);
-		SentryMessage().AppendLine().AppendLine("Sentry started!").Send();
+		DbgPrint("\n");                                       // (empty line in debug log for visual reference only)
+		SentryMessage().AppendLine("Sentry started!").Send(); // [Sentry] Sentry started!
 	}
 } // namespace sentry
 
