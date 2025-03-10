a6f21f93e61fa4ee39922e7f56e0dd2ced4aa122
JFresh <jfresh401@live.com>
2025-03-10 14:44:32 -0400

    Update SentrySync4_Sender.ino

--- Sentry Sync/SentrySync - Full Color/SentrySync4 - Controller/SentrySync4_Sender/SentrySync4_Sender.ino
+++ Sentry Sync/SentrySync - Full Color/SentrySync4 - Controller/SentrySync4_Sender/SentrySync4_Sender.ino
@@ -44,8 +44,8 @@ esp_now_peer_info_t peerInfo;
 
 
 
-const char* ssid = "Smiths";
-const char* password = "capitol401";
+const char* ssid = "YOUR NETWORK NAME";
+const char* password = "YOUR PASSWORD";
 
 WebServer server(80);
 
